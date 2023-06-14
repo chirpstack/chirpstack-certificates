a75246afecb997ca9dd1704faa8aea73cd34501a
Orne Brocaar <info@brocaar.com>
2023-06-14 15:06:58 +0100

    Update set-hosts.sh script.
    This removes some magic from the scripts, the script now accepts two
    arguments:
    
    1) the path to the (json) file to modify
    2) the hosts to set (e.g. host-one,host-two)

--- set-hosts.sh
+++ set-hosts.sh
@@ -1,33 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# -----------------------------------------------------------------------------
-# Defaults
-# -----------------------------------------------------------------------------
+set -e
 
-HOSTS=${HOSTS:-"127.0.0.1,localhost"}
-echo "Configuring certificates"
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then 
+	echo "Usage: $0 path/to/certificate.json host-one,host-two,host-three"
+	exit 0
+fi 
 
-# -----------------------------------------------------------------------------
-# Modify certificate.json files
-# -----------------------------------------------------------------------------
-
-hosts_for_service() {
-
-    local SERVICE=$1
-    local HOSTS_FOR_SERVICE=$(eval echo \$$SERVICE)
-    local HOSTS=${HOSTS_FOR_SERVICE:-$HOSTS}
-    echo "$HOSTS"
-
-}
-
-command_from_hosts() {
-    
+jq_hosts_command() {    
     local HOSTS=$1
-
     local INDEX=0
     local COMMAND=".hosts=[]"
     local HOSTS_LIST=$(echo ${HOSTS} | sed -n 1'p' | tr ',' '\n')
-    
     for HOST in ${HOSTS_LIST}
     do
         COMMAND="${COMMAND} | .hosts[${INDEX}]=\"${HOST}\""
@@ -35,16 +20,9 @@ command_from_hosts() {
     done
 
     echo "$COMMAND"
-
 }
 
-for FOLDER in `find config -name server -type d`
-do
-    SERVICE=$(echo $FOLDER | sed 's/[-\/]/_/g' | tr 'a-z' 'A-Z' | sed 's/_SERVER$/_HOSTS/' | sed 's/^CONFIG_//'  | sed 's/^CHIRPSTACK_//')
-    HOSTS_FOR_SERVICE=$(hosts_for_service "$SERVICE")
-    COMMAND=$(command_from_hosts "$HOSTS_FOR_SERVICE")
-    FILE=$FOLDER/certificate.json
-    echo "Changing hosts in $FILE to \"$HOSTS_FOR_SERVICE\""
-    contents=$(eval jq \'${COMMAND}\' $FILE)
-    echo "${contents}" > $FILE
-done
+echo "Configuring '${1}' with hosts '${2}'"
+COMMAND=$(jq_hosts_command "${2}")
+contents=$(eval jq \'${COMMAND}\' ${1})
+echo "${contents}" > ${1}
